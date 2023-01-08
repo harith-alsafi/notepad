91918d4c92ddd0c40c4060820c197280713e0bf5
Harith Al-Safi <harith.alsafi@gmail.com>
2023-01-08 14:48:05 +0300

    Added side bar

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:notepad/views/email_verify_view.dart';
 import 'package:notepad/views/error_view.dart';
 import 'package:notepad/views/initial_view.dart';
 import 'package:notepad/views/login_view.dart';
-import 'package:notepad/views/main_view.dart';
 import 'package:notepad/views/register_view.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:line_icons/line_icons.dart';
@@ -19,3 +18,5 @@ void main() {
     ),
   );
 }
+
+
